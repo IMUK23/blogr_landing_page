04b9e53e58f3f7885e2497ebc7d085a2d568ee00
Utkarsh Bahukhandi <utkarshbahukhandi0002@gmail.com>
2021-04-19 20:40:46 +0530

    direct

--- script/script.js
+++ script/script.js
@@ -1,12 +1,61 @@
 function change() {
     var x = document.getElementById("mobile-icon-id");
     var image = x.getElementsByClassName("hamburger")[0];
+    var menu = document.getElementById("menu");
 
-    if (image.src == "./images/icon-close.svg") {
+    if (image.src.match("./images/icon-close.svg")) {
         image.src = "./images/icon-hamburger.svg";
+        menu.style.display = "none";
+
     } else {
         image.src = "./images/icon-close.svg";
+        menu.style.display = "block";
+
     }
 
+
+}
+
+function product() {
+    if (document.getElementById("item-2-content").style.display.match("block")) {
+        hideproduct();
+    } else {
+        document.getElementById("item-2-content").style.display = "block";
+        document.getElementById("item-2-content").style.backgroundColor = "rgb(0,0,0)";
+        hidecompany();
+        hideconnect();
+    }
+}
+
+function hideproduct() {
+    document.getElementById("item-2-content").style.display = "none";
+}
+
+function company() {
+    if (document.getElementById("item-3-content").style.display.match("block")) {
+        hidecompany();
+    } else {
+        document.getElementById("item-3-content").style.display = "block";
+        hideproduct();
+        hideconnect();
+    }
+}
+
+function hidecompany() {
+    document.getElementById("item-3-content").style.display = "none";
+}
+
+function connect() {
+    if (document.getElementById("item-4-content").style.display.match("block")) {
+        hideconnect();
+    } else {
+        document.getElementById("item-4-content").style.display = "block";
+        hidecompany();
+        hideproduct();
+    }
+}
+
+function hideconnect() {
+    document.getElementById("item-4-content").style.display = "none";
 
 }
